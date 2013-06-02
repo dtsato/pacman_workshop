2036cefc28d1216415a0ad803a3856fdc42e81f1
Danilo Sato <dtsato@gmail.com>
2013-06-01 21:14:50 -0500

    Drawing dots

--- src/main/java/com/thoughtworks/pacman/ui/presenters/DotPresenter.java
+++ src/main/java/com/thoughtworks/pacman/ui/presenters/DotPresenter.java
@@ -4,14 +4,29 @@ import com.thoughtworks.pacman.core.Position;
 import com.thoughtworks.pacman.core.tiles.Dot;
 import com.thoughtworks.pacman.ui.Presenter;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 public class DotPresenter implements Presenter {
+    private static final int DIAMETER = 4;
+    private final Position position;
+
     public DotPresenter(Dot dot, Position position) {
+        this.position = position;
     }
 
     @Override
     public void draw(Graphics2D graphics) {
+        graphics.setColor(Color.white);
+        Rectangle bounds = getBounds();
+        graphics.fillOval(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
 
+    public Rectangle getBounds() {
+        int delta = (TILE_SIZE - DIAMETER) / 2;
+        Point point = position.times(TILE_SIZE).add(new Position(delta, delta)).toPoint();
+        return new Rectangle(point.x, point.y, DIAMETER, DIAMETER);
     }
 }
---
+++ src/test/java/com/thoughtworks/pacman/ui/presenters/DotPresenterTest.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.pacman.ui.presenters;
+
+import com.thoughtworks.pacman.core.Position;
+import org.junit.Test;
+
+import java.awt.Rectangle;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DotPresenterTest {
+    @Test
+    public void shouldCalculateDotBoundsInPixels() throws Exception {
+        DotPresenter presenter = new DotPresenter(null, new Position(2, 3));
+        assertThat(presenter.getBounds(), equalTo(new Rectangle(36, 52, 8, 8)));
+    }
+}
