de1eeb8232cc8c09bb3804dfd98cb96df07c567d
Danilo Sato <dtsato@gmail.com>
2013-06-12 20:57:50 -0700

    Avoid testing with big delta, and test for continuing on the same direction

--- src/test/java/com/thoughtworks/pacman/core/ActorTest.java
+++ src/test/java/com/thoughtworks/pacman/core/ActorTest.java
@@ -22,9 +22,9 @@ public class ActorTest {
         SpacialCoordinate center = new SpacialCoordinate(initialX, initialY);
         Actor actor = new Actor(maze, center, Direction.LEFT);
 
-        actor.advance(1000);
+        actor.advance(100);
 
-        assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX - 100, initialY)));
+        assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX - 10, initialY)));
     }
 
     @Test
@@ -34,7 +34,7 @@ public class ActorTest {
         SpacialCoordinate center = new SpacialCoordinate(initialX, initialY);
         Actor actor = new Actor(maze, center, Direction.UP);
 
-        actor.advance(1000);
+        actor.advance(100);
 
         assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX, initialY)));
     }
@@ -46,7 +46,7 @@ public class ActorTest {
         SpacialCoordinate center = new SpacialCoordinate(initialX, initialY);
         Actor actor = new Actor(maze, center, Direction.LEFT);
 
-        actor.advance(90);
+        actor.advance(100);
 
         assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX - 7, initialY)));
     }
@@ -59,9 +59,23 @@ public class ActorTest {
         Actor actor = new Actor(maze, center, Direction.LEFT);
 
         actor.setNextDirection(Direction.UP);
-        actor.advance(1000);
+        actor.advance(100);
 
-        assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX, initialY - 100)));
+        assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX, initialY - 10)));
         assertThat(actor.getDirection(), equalTo(Direction.UP));
+    }
+
+    @Test
+    public void advanceShouldKeepGoingInTheCurrentDirectionIfCantTurn() throws Exception {
+        int initialX = 8 * Tile.SIZE - Tile.SIZE / 2;
+        int initialY = 26 * Tile.SIZE + Tile.SIZE / 2;
+        SpacialCoordinate center = new SpacialCoordinate(initialX, initialY);
+        Actor actor = new Actor(maze, center, Direction.LEFT);
+
+        actor.setNextDirection(Direction.UP);
+        actor.advance(100);
+
+        assertThat(actor.getCenter(), equalTo(new SpacialCoordinate(initialX - 10, initialY)));
+        assertThat(actor.getDirection(), equalTo(Direction.LEFT));
     }
 }
