21961935f251d77645a277c3d9b832ad97c04ecb
Danilo Sato <dtsato@gmail.com>
2013-06-25 16:10:27 -0300

    Adding test for TileToPresenterFactory

---
+++ src/test/java/com/thoughtworks/pacman/ui/TileToPresenterFactoryTest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.pacman.ui;
+
+import com.thoughtworks.pacman.core.tiles.Dot;
+import com.thoughtworks.pacman.core.tiles.EmptyTile;
+import com.thoughtworks.pacman.core.tiles.Wall;
+import com.thoughtworks.pacman.ui.presenters.DotPresenter;
+import com.thoughtworks.pacman.ui.presenters.NullPresenter;
+import com.thoughtworks.pacman.ui.presenters.WallPresenter;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class TileToPresenterFactoryTest {
+    @Test
+    public void toPresenter_shouldReturnWallPresenter_whenTileIsWall() {
+        Presenter presenter = TileToPresenterFactory.toPresenter(new Wall(null));
+        assertThat(presenter, instanceOf(WallPresenter.class));
+    }
+
+    @Test
+    public void toPresenter_shouldReturnDotPresenter_whenTileIsDot() {
+        Presenter presenter = TileToPresenterFactory.toPresenter(new Dot(null));
+        assertThat(presenter, instanceOf(DotPresenter.class));
+    }
+
+    @Test
+    public void toPresenter_shouldReturnNullPresenter_whenTileIsEmptyTile() {
+        Presenter presenter = TileToPresenterFactory.toPresenter(new EmptyTile(null));
+        assertThat(presenter, instanceOf(NullPresenter.class));
+    }
+}
