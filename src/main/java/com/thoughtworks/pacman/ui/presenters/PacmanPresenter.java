a5c6ac350d9dc62e6a10938e5751e32a758f8604
Mariana Bravo <mbravo@thoughtworks.com>
2013-06-25 23:08:25 -0300

    Larger pacman

--- src/main/java/com/thoughtworks/pacman/ui/presenters/PacmanPresenter.java
+++ src/main/java/com/thoughtworks/pacman/ui/presenters/PacmanPresenter.java
@@ -1,17 +1,17 @@
 package com.thoughtworks.pacman.ui.presenters;
 
-import com.thoughtworks.pacman.core.Direction;
-import com.thoughtworks.pacman.core.SpacialCoordinate;
-import com.thoughtworks.pacman.core.Tile;
-import com.thoughtworks.pacman.core.actors.Pacman;
-import com.thoughtworks.pacman.ui.Presenter;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
+import com.thoughtworks.pacman.core.Direction;
+import com.thoughtworks.pacman.core.SpacialCoordinate;
+import com.thoughtworks.pacman.core.actors.Pacman;
+import com.thoughtworks.pacman.ui.Presenter;
+
 public class PacmanPresenter implements Presenter {
+    private static final int DIAMETER = 20;
     static final int MOUTH_CLOSED = 360;
     static final int MOUTH_OPENED = 280;
 
@@ -46,8 +46,8 @@ public class PacmanPresenter implements Presenter {
     }
 
     Rectangle getBounds() {
-        int radius = Tile.SIZE / 2;
+        int radius = DIAMETER / 2;
         SpacialCoordinate upperLeft = pacman.getCenter().add(new SpacialCoordinate(-radius, -radius));
-        return new Rectangle(upperLeft.toPoint(), new Dimension(Tile.SIZE, Tile.SIZE));
+        return new Rectangle(upperLeft.toPoint(), new Dimension(DIAMETER, DIAMETER));
     }
 }
