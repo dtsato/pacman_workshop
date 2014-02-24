f41a11617ef2ce63bd65ae567e850ce270c6d15e
Mariana Bravo <marivb@gmail.com>
2014-02-24 11:53:28 -0500

    Removing GamePresenter dependency on specific ghost types

--- src/main/java/com/thoughtworks/pacman/ui/presenters/GamePresenter.java
+++ src/main/java/com/thoughtworks/pacman/ui/presenters/GamePresenter.java
@@ -1,24 +1,25 @@
 package com.thoughtworks.pacman.ui.presenters;
 
 import com.thoughtworks.pacman.core.Game;
+import com.thoughtworks.pacman.core.actors.Ghost;
 import com.thoughtworks.pacman.ui.Presenter;
 
 import java.awt.Graphics2D;
+import java.util.LinkedList;
+import java.util.List;
 
 public class GamePresenter implements Presenter {
     private final MazePresenter mazePresenter;
     private final PacmanPresenter pacmanPresenter;
-    private final GhostPresenter[] ghostPresenters;
+    private final List<GhostPresenter> ghostPresenters;
 
     public GamePresenter(Game game) {
         mazePresenter = new MazePresenter(game.getMaze());
         pacmanPresenter = new PacmanPresenter(game.getPacman());
-        ghostPresenters = new GhostPresenter[] {
-                new GhostPresenter(game.getBlinky()),
-                new GhostPresenter(game.getPinky()),
-                new GhostPresenter(game.getInky()),
-                new GhostPresenter(game.getClyde())
-        };
+        ghostPresenters = new LinkedList<>();
+        for (Ghost ghost : game.getGhosts()) {
+            ghostPresenters.add(new GhostPresenter(ghost));
+        }
     }
 
     public void draw(Graphics2D graphics) {
